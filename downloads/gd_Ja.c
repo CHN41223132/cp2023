20a508350fb202f31f05768bdf7dd5a0fb6c4304
CHN41223132 <144768416+CHN41223132@users.noreply.github.com>
2024-01-08 08:38:36 +0000

    日更新

--- downloads/gd_Ja.c
+++ downloads/gd_Ja.c
@@ -1,59 +1,69 @@
-  #include <stdio.h>
-  #include <gd.h>
-  #include <math.h>
+#include <stdio.h>
+#include <gd.h>
+#include <math.h>
 
-  void draw_japan_flag(gdImagePtr img);
-  void draw_red_sun(gdImagePtr img, int x, int y, int size, int color);
+void draw_japan_flag(gdImagePtr img);
+void draw_red_sun(gdImagePtr img, int x, int y, int size, int color);
 
-  int main() {
-  int originalWidth = 1200;
-  int originalHeight = (int)(originalWidth * 2.0 / 3.0);
-  gdImagePtr img = gdImageCreateTrueColor(originalWidth, originalHeight);
-  gdImageAlphaBlending(img, 0);
+int main() {
+    // 旗幟原始寬度
+    int originalWidth = 1200;
+    // 旗幟原始高度
+    int originalHeight = (int)(originalWidth * 2.0 / 3.0);
 
-  draw_japan_flag(img);
+    // 創建原始尺寸的圖像
+    gdImagePtr img = gdImageCreateTrueColor(originalWidth, originalHeight);
+    gdImageAlphaBlending(img, 0);
 
-  // 新的宽度和高度以适应 "images" 文件夹
-  int newWidth = 600;
-  int newHeight = (int)(newWidth * 2.0 / 3.0);
+    // 繪製日本國旗
+    draw_japan_flag(img);
 
-  // 创建新图像并进行缩放
-  gdImagePtr resizedImage = gdImageCreateTrueColor(newWidth, newHeight);
-  gdImageAlphaBlending(resizedImage, 0);
-  gdImageCopyResampled(resizedImage, img, 0, 0, 0, 0, newWidth, newHeight, originalWidth, originalHeight);
+    // 新的寬度和高度以符合 "images" 資料夾
+    int newWidth = 510; // 新的旗幟寬度
+    int newHeight = (int)(newWidth * 2.0 / 3.0); // 新的旗幟高度
 
-  FILE *outputFile = fopen("./../images/japan_flag.png", "wb");
-  if (outputFile == NULL) {
-  fprintf(stderr, "Error opening the output file.\n");
-  return 1;
-  }
-  gdImagePng(resizedImage, outputFile);
-  fclose(outputFile);
-  gdImageDestroy(img);
-  gdImageDestroy(resizedImage);
+    // 創建新尺寸的圖像並進行縮放
+    gdImagePtr resizedImage = gdImageCreateTrueColor(newWidth, newHeight);
+    gdImageAlphaBlending(resizedImage, 0);
+    gdImageCopyResampled(resizedImage, img, 0, 0, 0, 0, newWidth, newHeight, originalWidth, originalHeight);
 
-  return 0;
-  }
+    // 開啟輸出檔案
+    FILE *outputFile = fopen("./../images/japan_flag.png", "wb");
+    if (outputFile == NULL) {
+        fprintf(stderr, "Error opening the output file.\n");
+        return 1;
+    }
 
-  void draw_japan_flag(gdImagePtr img) {
-  int width = gdImageSX(img);
-  int height = gdImageSY(img);
+    // 將新尺寸的圖像寫入檔案
+    gdImagePng(resizedImage, outputFile);
+    fclose(outputFile);
 
-  // 创建一个白色背景
-  int white = gdImageColorAllocate(img, 255, 255, 255);
-  gdImageFilledRectangle(img, 0, 0, width - 1, height - 1, white);
+    // 釋放記憶體
+    gdImageDestroy(img);
+    gdImageDestroy(resizedImage);
 
-  // 绘制红色圆圈（太阳）
-  int red = gdImageColorAllocate(img, 255, 0, 0);
-  int center_x = width / 2;
-  int center_y = height / 2;
-  int radius = (int)((width * 2) / 3);
-  draw_red_sun(img, center_x, center_y, radius, red);
-  }
+    return 0;
+}
 
-  void draw_red_sun(gdImagePtr img, int x, int y, int size, int color) {
-  // 減小 size 的值,例如將他的值減半
-  size = size / 2;
-  gdImageArc(img, x, y, size, size, 0, 360, color);
-  gdImageFillToBorder(img, x, y, color, color);
-  }
+void draw_japan_flag(gdImagePtr img) {
+    int width = gdImageSX(img);
+    int height = gdImageSY(img);
+
+    // 創建一個白色背景
+    int white = gdImageColorAllocate(img, 255, 255, 255);
+    gdImageFilledRectangle(img, 0, 0, width - 1, height - 1, white);
+
+    // 繪製紅色圓圈（太陽）
+    int red = gdImageColorAllocate(img, 255, 0, 0);
+    int center_x = width / 2;
+    int center_y = height / 2;
+    int radius = (int)((width * 2) / 3);
+    draw_red_sun(img, center_x, center_y, radius, red);
+}
+
+void draw_red_sun(gdImagePtr img, int x, int y, int size, int color) {
+    // 減小 size 的值，例如將其值減半
+    size = size / 2;
+    gdImageArc(img, x, y, size, size, 0, 360, color);
+    gdImageFillToBorder(img, x, y, color, color);
+}
